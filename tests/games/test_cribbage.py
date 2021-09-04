e4ce9dacbce1f774def360cf477716e3e5db231c
Jim Barrett <jimbarrett27@gmail.com>
2021-09-04 21:56:50 +0200

    remove old style hands from cribbage tests

--- tests/games/test_cribbage.py
+++ tests/games/test_cribbage.py
@@ -5,44 +5,12 @@ from pycards.players import Players
 def make_basic_cribbage_game(n_players: int):
 
     players = Players([
-        CribbagePlayer(is_dealer=True, name='Alice'),
-        CribbagePlayer(is_dealer=False, name="Bob"),
-        CribbagePlayer(is_dealer=False, name="Charlie"),
-        CribbagePlayer(is_dealer= False, name="Dave"),
+        CribbagePlayer(is_dealer=True, name='Alice', seat_position=1),
+        CribbagePlayer(is_dealer=False, name="Bob", seat_position=2),
+        CribbagePlayer(is_dealer=False, name="Charlie", seat_position=3),
+        CribbagePlayer(is_dealer= False, name="Dave", seat_position=4),
     ][:n_players])
 
     return Cribbage(players)
 
-def test_score_hand():
 
-    game = make_basic_cribbage_game(2)
-
-    hand = Cards([
-        Card.from_strings('Five', 'Hearts'),
-        Card.from_strings('Five', 'Diamonds'),
-        Card.from_strings('Five', 'Spades'),
-        Card.from_strings('Jack', 'Clubs')
-    ])
-    game.turn_up_card = Card.from_strings('Five', 'Clubs')
-
-    assert game._score_hand(hand) == 29
-
-    hand = Cards([
-        Card.from_strings('Ace', 'Hearts'),
-        Card.from_strings('Two', 'Diamonds'),
-        Card.from_strings('Three', 'Spades'),
-        Card.from_strings('Four', 'Clubs')
-    ])
-    game.turn_up_card = Card.from_strings('Five', 'Clubs')
-
-    assert game._score_hand(hand) == 7
-
-    hand = Cards([
-        Card.from_strings('Ace', 'Hearts'),
-        Card.from_strings('Two', 'Hearts'),
-        Card.from_strings('Three', 'Hearts'),
-        Card.from_strings('Four', 'Hearts')
-    ])
-    game.turn_up_card = Card.from_strings('Five', 'Hearts')
-
-    assert game._score_hand(hand) == 12
